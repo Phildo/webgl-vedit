0c22a0e7ebb14c51242c309c2311521779f7927d
Phildo <Phildo211@gmail.com>
2012-12-15 01:05:27 +0000

    deleted stuff for new refactor

--- GL/XGLGeo.js
+++
@@ -1,9 +0,0 @@
-var XGLGeo = function (verts, colors, normals, indexes)
-{
-  this.verts = verts;
-  this.colors = colors;
-  this.normals = normals;
-  this.indexes = indexes;
-
-  this.programGeoListIndex = -1; //Used entirely by XGLPrograms. Should be otherwise ignored.
-}
